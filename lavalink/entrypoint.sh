f516ebc2fea27c34baa265046677c22280868df0
Plain <66025383+PlainDevelopment@users.noreply.github.com>
2022-12-31 00:39:07 +0000

    Update entrypoint.sh

--- lavalink/entrypoint.sh
+++ lavalink/entrypoint.sh
@@ -1,8 +1,18 @@
 #!/bin/ash
-cd /home/container
 
-MODIFIED_STARTUP=$(echo -e $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
-echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+cd /home/container;
 
-# Run the Server
-eval ${MODIFIED_STARTUP}
+if [[ ! -f Lavalink.jar ]]; then
+	curl -L -o Lavalink.jar https://github.com/freyacodes/Lavalink/releases/latest/download/Lavalink.jar
+fi;
+
+if [[ ! -f application.yml ]]; then
+	curl -L -o application.yml https://raw.githubusercontent.com/freyacodes/Lavalink/master/LavalinkServer/application.yml.example
+fi;
+
+if [[ $(md5sum Lavalink.jar) =~ 87a0b32723282d074081daac41877f59 ]]; then
+	echo Approved;
+	java -jar Lavalink.jar
+else
+	echo Lavalink.jar has been tampered with. Please download version 3.6.2 from GitHub.;
+fi
