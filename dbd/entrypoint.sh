a346248b48cdc4cd0656e9eef5ac95f9014bca23
Plain <66025383+PlainDevelopment@users.noreply.github.com>
2022-12-31 21:11:46 +0000

    Update entrypoint.sh

--- dbd/entrypoint.sh
+++ dbd/entrypoint.sh
@@ -5,8 +5,18 @@ cd /home/container
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-# Replace Startup Variables
-MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+npm i ${DBD_THEME}@latest discord-dashboard@${DBD_VERSION} discord.js@${DISCORD_VERSION} mongoose knex;
+if [ ! -f index.js ]; then
+    echo -e "Downloading index.js";
+    curl -o index.js https://mydbd.xyz/assets/js/${DBD_THEME}.js;
+fi;
 
-# Run the Server
-eval ${MODIFIED_STARTUP}
+sed -i "s/{THEME}/${DBD_THEME}/g" /home/container/index.js;
+sed -i "s/{TOKEN}/${DISCORD_TOKEN}/g" /home/container/index.js;
+sed -i "s/{DBD_LICENSE}/${DBD_LICENSE}/g" /home/container/index.js;
+sed -i "s/{PORT}/${SERVER_PORT}/g" /home/container/index.js;
+sed -i "s/{CLIENT_ID}/${CLIENT_ID}/g" /home/container/index.js;
+sed -i "s/{CLIENT_SECRET}/${CLIENT_SECRET}/g" /home/container/index.js;
+
+echo -e "Free discord-dashboard hosting - mydbd.xyz";
+/usr/local/bin/node /home/container/index.js
