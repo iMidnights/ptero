04662459e233313d4f1d7982ee707aa362a394d2
iMidnight <mohaidarus@gmail.com>
2023-04-13 16:02:34 +0100

    Update

--- vaultwarden/entrypoint.sh
+++ vaultwarden/entrypoint.sh
@@ -6,7 +6,8 @@ INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
 # Replace Startup Variables
-MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-eval docker run -d --name vaultwarden -v /vw-data/:/data/ -p 1026:1026 vaultwarden/server:latest
+eval ${MODIFIED_STARTUP}
