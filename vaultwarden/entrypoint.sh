38e7a55a634e2f24daeb6528709c9862b7d8dc0e
iMidnight <mohaidarus@gmail.com>
2023-04-13 16:19:29 +0100

    Update entrypoint.sh

--- vaultwarden/entrypoint.sh
+++ vaultwarden/entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-eval docker run -d --name vaultwarden -v /vw-data/:/data/ -p 1026:1026 vaultwarden/server:latest
+cd /home/container
 
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
 
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
